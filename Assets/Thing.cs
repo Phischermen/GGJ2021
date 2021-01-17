4acff46c77f71af8ca790c9b10258845584a25de
Seven-Four <77557157+Seven-Four@users.noreply.github.com>
2021-01-16 17:52:04 -0700

    Thing

---
+++ Assets/Thing.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("It did a thing");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
