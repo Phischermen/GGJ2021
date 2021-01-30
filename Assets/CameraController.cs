c514f07d4095b964777a7819cc2cff8ef80df66b
Phischermen <kevfischermen@gmail.com>
2021-01-29 22:33:04 -0800

    Added camera controller

---
+++ Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject Boat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Boat = GameObject.FindGameObjectWithTag("Boat");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Boat.transform.position;
+    }
+}
