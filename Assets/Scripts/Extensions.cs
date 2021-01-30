e81d6e6de5f6cb4c642ee87a64e8e1f415fbbebe
Phischermen <kevfischermen@gmail.com>
2021-01-30 10:46:19 -0800

    Added perpDotProduct to Extensions

--- Assets/Scripts/Extensions.cs
+++ Assets/Scripts/Extensions.cs
@@ -17,5 +17,9 @@ namespace Extensions
                 cell.y > array.GetUpperBound(1)) return false;
             return true;
         }
+        public static float perpDotProduct(this Vector2 v1, Vector2 v2)
+        {
+            return v1.x * v2.y - v1.y * v2.x;
+        }
     }
 }
