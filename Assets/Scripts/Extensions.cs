c2f7d9f1e6fd31d204b19556639c51c089283d99
Phischermen <kevfischermen@gmail.com>
2021-01-30 09:23:50 -0800

    Added extension for 2d arrays

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Extensions
+{
+    public static class ExtensionMethods
+    {
+        public static bool In2DArrayBounds(this object[,] array, Vector2Int cell)
+        {
+            if (cell.x < array.GetLowerBound(0) ||
+                cell.x > array.GetUpperBound(0) ||
+                cell.y < array.GetLowerBound(1) ||
+                cell.y > array.GetUpperBound(1)) return false;
+            return true;
+        }
+    }
+}
