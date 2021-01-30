c029f2a5e1c537e3c3937106522e976fa350f549
Seven-Four <77557157+Seven-Four@users.noreply.github.com>
2021-01-30 14:29:27 -0700

    Base Wind
    Created script for wind direction and force. Also wind indicator

---
+++ Assets/Wind.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    public float maxWind = 10;
+    public float windForce;
+    public float targetForce;
+    public Vector2 windDirection;
+    public Vector2 targetDirection;
+    public Vector2 windChangeRange = new Vector2(3, 5);
+    public Vector2 windVector;
+    public BoatSteering boat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boat = boat == null ? GameObject.FindWithTag("Boat").GetComponent<BoatSteering>() : boat;
+        ChangeWind();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = boat.transform.position;
+        windDirection = Vector3.Lerp(windDirection, targetDirection, .9f * Time.deltaTime);
+        transform.up = windDirection;
+        windForce = Mathf.Lerp(windForce, targetForce, .9f * Time.deltaTime);
+        windVector = windForce * windDirection;
+    }
+
+    void ChangeWind()
+    {
+        targetForce = Random.Range(0, maxWind);
+        targetDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+        StartCoroutine(windChangeDelay(Random.Range(windChangeRange.x, windChangeRange.y)));
+    }
+
+    IEnumerator windChangeDelay(float seconds)
+    {
+        Debug.Log("Change wind");
+        yield return new WaitForSeconds(seconds);
+        ChangeWind();
+    }
+}
