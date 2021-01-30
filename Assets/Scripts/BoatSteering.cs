f6a31a87d5bf262275c0f37146d6825b69682854
Seven-Four <77557157+Seven-Four@users.noreply.github.com>
2021-01-29 22:57:31 -0700

    Simple Boat Movement and Prefab
    Boat moves while sail is open and can turn while rudders work.
    
    Steer with Left/Right arrows or A and D
    Disable/Enable sail with Left Click
    Disable/Enable rudder with Right Click

---
+++ Assets/Scripts/BoatSteering.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatSteering : MonoBehaviour
+{
+    public float baseSpeed = 10;
+    public float turnSpeed = 5;
+    [SerializeField]
+    bool rudderWorking = true;
+    [SerializeField]
+    bool sailOpen = true;
+
+    public Vector2 moveVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Test Script: Open/Close sail with Left Click
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SailOpenClose();
+        }
+        //Test Script: Break/Fix rudder with Right Click
+        if (Input.GetButtonDown("Fire2"))
+        {
+            RudderBreakFix();
+        }
+        if (sailOpen)
+        {
+            moveVector = transform.up * baseSpeed * Time.deltaTime;
+            transform.position = new Vector3(transform.position.x + moveVector.x, transform.position.y + moveVector.y, transform.position.z);
+        }
+        if (rudderWorking)
+        {
+            float rotation = Input.GetAxis("Horizontal");
+            if (Mathf.Abs(rotation) > .1)
+            {
+                transform.Rotate(Vector3.back * rotation * turnSpeed * Time.deltaTime);
+            }
+        }
+    }
+
+    public void SailOpenClose()
+    {
+        sailOpen = !sailOpen;
+    }
+
+    public void RudderBreakFix()
+    {
+        rudderWorking = !rudderWorking;
+    }
+}
