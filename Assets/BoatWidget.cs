5b7fa16abdb4994193743666e2bc79efdc778162
Phischermen <kevfischermen@gmail.com>
2021-01-30 13:09:02 -0800

    Implemented the rest of the boat widget callbacks.

--- Assets/BoatWidget.cs
+++ Assets/BoatWidget.cs
@@ -41,15 +41,18 @@ public class BoatWidget : MonoBehaviour
     {
         targetDeckPosition = helmStationPosition;
         currentDeckPosition = helmStationPosition;
+        RudderButton = GameObject.Find("Rudder").GetComponent<Button>();
         HelmButton = GameObject.Find("Helm").GetComponent<Button>();
         SailButton = GameObject.Find("Sail").GetComponent<Button>();
         LanternButton = GameObject.Find("Lantern").GetComponent<Button>();
-        RudderButton = GameObject.Find("Rudder").GetComponent<Button>();
         TendButton = GameObject.Find("Tend").GetComponent<Button>();
 
         // Setup callbacks
+        RudderButton.onClick.AddListener(RudderButtonPressed);
         HelmButton.onClick.AddListener(HelmButtonPressed);
         SailButton.onClick.AddListener(SailButtonPressed);
+        LanternButton.onClick.AddListener(LanternButtonPressed);
+        TendButton.onClick.AddListener(TendButtonPressed);
     }
 
     // Update is called once per frame
@@ -70,7 +73,7 @@ public class BoatWidget : MonoBehaviour
         if (currentDeckPosition == targetDeckPosition && currentStation != targetStation)
         {
             currentStation = targetStation;
-            Debug.Log("Station Reached");
+            Debug.Log("Station Reached: " + currentStation);
             //switch (currentStation)
             //{
             //    case Station.helm:
@@ -84,9 +87,15 @@ public class BoatWidget : MonoBehaviour
             //}
         }
 
-        Debug.Log(currentDeckPosition);
+        //Debug.Log(currentDeckPosition);
     }
 
+    public void RudderButtonPressed()
+    {
+        targetStation = Station.rudder;
+        currentStation = Station.noStation;
+        targetDeckPosition = rudderStationPosition;
+    }
     public void HelmButtonPressed()
     {
         targetStation = Station.helm;
@@ -98,5 +107,17 @@ public class BoatWidget : MonoBehaviour
         targetStation = Station.sail;
         currentStation = Station.noStation;
         targetDeckPosition = sailStationPosition;
+    }
+    public void LanternButtonPressed()
+    {
+        targetStation = Station.lantern;
+        currentStation = Station.noStation;
+        targetDeckPosition = lanternStationPosition;
+    }
+    public void TendButtonPressed()
+    {
+        targetStation = Station.tend;
+        currentStation = Station.noStation;
+        targetDeckPosition = tendStationPosition;
     }
 }
