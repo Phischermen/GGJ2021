7ba95ed11fdd4e7f44c2cddc33ad97f4963af11e
ZacharyFFuller <50885386+ZacharyFFuller@users.noreply.github.com>
2021-01-31 16:48:26 -0500

    Updated all the buttons so now they work, except one...

--- Assets/MenuActions.cs
+++ Assets/MenuActions.cs
@@ -7,6 +7,16 @@ public class MenuActions : MonoBehaviour
 {
     public void Play()
     {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    public void Return()
+    {
         SceneManager.LoadScene(0);
     }
 
