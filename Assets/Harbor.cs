4f6ca910f7b6a60ddc8517f3fd543a5df6954919
Seven-Four <77557157+Seven-Four@users.noreply.github.com>
2021-01-30 21:34:08 -0700

    Basic Win Condition
    When the player reaches the harbor the GameMaster will load the win scene (currently just reloads game scene)
    
    GameInit also creates GameMaster and Wind prefabs.
    
    Added Harbor script

---
+++ Assets/Harbor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Harbor : MonoBehaviour
+{
+    public GameMaster gameMaster;
+
+    private void Start()
+    {
+        
+    }
+
+    
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Boat")
+        {
+            Debug.Log("Trigger win");
+            gameMaster.EndGame(true, 0);
+        }
+    }
+}
