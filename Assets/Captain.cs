08a7112667392d3e69575eca03283e98742226a4
Phischermen <kevfischermen@gmail.com>
2021-03-06 10:57:14 -0800

    Integrated captain noises

--- Assets/Captain.cs
+++ Assets/Captain.cs
@@ -10,18 +10,38 @@ public class Captain : MonoBehaviour
     [Range(0, 1)]
     public float fallChance = .5f;
 
+    // Initialized in inspector
     public Sprite noCaptain;
     public SpriteRenderer ship;
+    public string captainDownClipDirectory;
+    public string captainTendedClipDirectory;
 
+    // Initialized via BindBoatToBoatWidget()
+    [HideInInspector]
     public Animator anim;
+    [HideInInspector]
     public Image img;
+
     public Sprite awakeSpr;
     public Sprite sleepingSpr;
 
+    new public AudioSource audio;
+
+    private AudioClip[] captainDownClips;
+    private AudioClip[] captainTendedClips;
+    private int tic1;
+    private int tic2;
+    public void Start()
+    {
+        captainDownClips = Resources.LoadAll<AudioClip>("Sound/captain_needs_tending");
+        captainTendedClips = Resources.LoadAll<AudioClip>("Sound/captain_tended");
+    }
     public void Wake()
     {
         awake = true;
         img.sprite = awakeSpr;
+        IncrementTic(ref tic1, captainTendedClips);
+        audio.PlayOneShot(captainTendedClips[tic1]);
     }
 
     public void Sleep()
@@ -30,6 +50,8 @@ public class Captain : MonoBehaviour
         if (awake == true)
         {
             awake = false;
+            IncrementTic(ref tic2, captainTendedClips);
+            audio.PlayOneShot(captainDownClips[tic2]);
             img.sprite = sleepingSpr;
         }
         else if(Random.value <= fallChance)
@@ -45,6 +67,15 @@ public class Captain : MonoBehaviour
             onBorad = false;
             ship.sprite = noCaptain;
             anim.Play("CaptainSlide");
+        }
+    }
+
+    private void IncrementTic(ref int tic, object[] audioSources)
+    {
+        tic += 1;
+        if (tic >= audioSources.Length)
+        {
+            tic = 0;
         }
     }
 }
