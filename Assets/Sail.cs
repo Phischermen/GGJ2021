f2556618bc4d9229fe22a29befea2433137b5168
Seven-Four <77557157+Seven-Four@users.noreply.github.com>
2021-03-18 22:28:50 -0600

    Sail Clamp
    Sail now clamps and cannot be turned past 90 or -90

--- Assets/Sail.cs
+++ Assets/Sail.cs
@@ -50,6 +50,10 @@ public class Sail : MonoBehaviour
             if (Mathf.Abs(rotation) > .1)
             {
                 transform.Rotate(Vector3.forward * rotation * turnSpeed * Time.deltaTime);
+                float rotationZ = transform.localRotation.eulerAngles.z;
+                rotationZ = rotationZ > 180? Mathf.Clamp(rotationZ, 270, 360) : Mathf.Clamp(rotationZ, 0, 90);
+                Debug.Log("pre clamp: " + transform.localRotation.eulerAngles.z + "post clamp: " + rotationZ);
+                transform.localRotation = Quaternion.Euler(0, 0, rotationZ);
             }
         }
     }
