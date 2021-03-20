bbc3617ecc358cf68cbe1a1ed16113079169ebb0
Phischermen <kevfischermen@gmail.com>
2021-03-20 16:13:23 -0700

    Updated instruction manual

--- Assets/InstructionManual.cs
+++ Assets/InstructionManual.cs
@@ -70,7 +70,7 @@ public class InstructionManual : MonoBehaviour
     {
         // Manual starts closed
         GetComponent<Canvas>().enabled = false;
-        manualPages = Resources.LoadAll<Sprite>("Sprites/InstructionManual/Manual");
+        manualPages = Resources.LoadAll<Sprite>("Sprites/InstructionManual/Manual01");
         Page = 0;
         next.onClick.AddListener(NextPressed);
         prev.onClick.AddListener(PrevPressed);
