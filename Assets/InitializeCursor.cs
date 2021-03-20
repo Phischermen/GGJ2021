72f4cac2c42e7f8e6c84d6147a19393d51c07cc2
Phischermen <kevfischermen@gmail.com>
2021-03-20 16:13:05 -0700

    Added a new mouse cursor

---
+++ Assets/InitializeCursor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitializeCursor : MonoBehaviour
+{
+    public Texture2D cursor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(cursor, new Vector2(94, 71),CursorMode.ForceSoftware);
+    }
+}
