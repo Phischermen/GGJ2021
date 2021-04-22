70f047811f247e69395c13fd602ad495aa250fc5
ZacharyFFuller <50885386+ZacharyFFuller@users.noreply.github.com>
2021-04-22 14:33:05 -0400

    Barrels move
    I made 2 scripts, one for objects following a path, and one for objects to orbit around each other

---
+++ Assets/Resources/Sprites/seaObstacles/individualSprites/TESTING SPRITES/OrbitAround.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitAround : MonoBehaviour
+{
+
+    public GameObject pivot;
+    public float speed = 45;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Orbit();
+    }
+
+    void Orbit()
+    {
+        transform.RotateAround(pivot.transform.position, Vector3.forward, speed * Time.deltaTime);
+
+        transform.Rotate(new Vector3(0f, 0f, (speed * Time.deltaTime)*-1));
+
+    }
+
+}
