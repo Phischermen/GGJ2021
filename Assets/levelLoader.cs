46259d414ea2f0f1e30e790c275e6579b3954949
ZacharyFFuller <50885386+ZacharyFFuller@users.noreply.github.com>
2021-05-20 13:10:36 -0400

    Added LevelLoader and Moving Hazard Damage
    There is now a basic fade-in fade-out level transition that just needs to be hooked up to the menu buttons and the game winning/losing
    
    Also the moving hazards should now deal damage properly

---
+++ Assets/levelLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelLoader : MonoBehaviour
+{
+
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        StartCoroutine(LoadLevelTransition(levelName));
+    }
+
+    IEnumerator LoadLevelTransition(string levelName)
+    {
+        transition.SetTrigger("start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelName);
+    }
+
+}
