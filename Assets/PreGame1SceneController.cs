6b362417e76e5e0e28cc39e156016d7b302e7340
Phischermen <kevfischermen@gmail.com>
2021-06-06 12:04:56 -0700

    Limmerick is now skippable

--- Assets/PreGame1SceneController.cs
+++ Assets/PreGame1SceneController.cs
@@ -10,10 +10,26 @@ public class PreGame1SceneController : MonoBehaviour
     string message;
     public float speed = 0.09f;
     public float newLinePause = 0.2f;
-    bool WantsToStartGame;
+    bool wantsToStartGame;
     public GameObject PlayButton;
+    float timeUntilSkipPossible = 1f;
+    bool wantsToSkipLimmerick;
+    
+    void Start()
+    {
+        StartCoroutine(nameof(LimmerickDisplay));
+    }
 
-    IEnumerator Start()
+    private void Update()
+    {
+        timeUntilSkipPossible -= Time.deltaTime;
+        if (Input.anyKeyDown && timeUntilSkipPossible < 0)
+        {
+            wantsToSkipLimmerick = true;
+        }
+    }
+
+    IEnumerator LimmerickDisplay()
     {
         StartCoroutine(nameof(LoadGamePlayScene));
         PlayButton.SetActive(false);
@@ -27,16 +43,20 @@ public class PreGame1SceneController : MonoBehaviour
             if (character != '\n')
             {
                 textComponent.text += character;
-                yield return waitCharacter;
+                // Pressing any key causes full line to be displayed
+                if (!wantsToSkipLimmerick) yield return waitCharacter;
             }
             else
             {
                 yield return waitNewLine;
+                wantsToSkipLimmerick = false;
                 textComponent.text += character;
             }
         }
+        textComponent.text = message;
         PlayButton.SetActive(true);
     }
+
     IEnumerator LoadGamePlayScene()
     {
         loadScene = SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
@@ -44,16 +64,17 @@ public class PreGame1SceneController : MonoBehaviour
         while (!loadScene.isDone)
         {
             //Debug.Log(loadScene.progress);
-            if (WantsToStartGame)
+            if (wantsToStartGame)
             {
                 loadScene.allowSceneActivation = true;
             }
             yield return null;
         }
     }
+
     public void StartGame()
     {
         Debug.Log("(ô_ô)");
-        WantsToStartGame = true;
+        wantsToStartGame = true;
     }
 }
